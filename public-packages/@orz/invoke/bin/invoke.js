1985d82df73816b87fa335596dd0eda7d42fa94b
Yeechan Lu <git@orzfly.com>
2020-02-24 17:16:27 +0000

    Fix invoke bin order

--- public-packages/@orz/invoke/bin/invoke.js
+++ public-packages/@orz/invoke/bin/invoke.js
@@ -13,7 +13,6 @@ const lib = (() => {
   return null ||
     (env1 && tryRequire(env1)) ||
     tryRequire('../dist-dev') ||
-    tryRequire('../lib') ||
     tryRequire('../dist') ||
     (env2 && tryRequire(env2)) ||
     (() => { throw new Error('Cannot find usable invoke lib!') })()
